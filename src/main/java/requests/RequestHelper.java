37868e70960d4b1028b78783f0db6bdc30102a05
Ian Cavanaugh <icavanaugh95@gmail.com>
2018-10-05 18:21:46 -0400

    changed response

--- src/main/java/requests/RequestHelper.java
+++ src/main/java/requests/RequestHelper.java
@@ -10,5 +10,10 @@ public class RequestHelper {
 	public static void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String uri = request.getRequestURI();
 		System.out.println(uri);
+		
+		if(uri.equals("/Project2/Servlet/getSomething")) {
+			System.out.println("made it here");
+			response.getWriter().append("[{\"name\":\"Adam\",\"Age\":19},{\"name\":\"Brian\",\"Age\":24},{\"name\":\"Jackie\",\"Age\":23}]");
+		}
 	}
 }
