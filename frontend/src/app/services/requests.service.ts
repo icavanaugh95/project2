badcafa3f77e20999d72c04bfce7272d647d1e3f
Ian Cavanaugh <icavanaugh95@gmail.com>
2018-10-05 18:22:18 -0400

    started servlet integration

---
+++ frontend/src/app/services/requests.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestsService {
+
+  constructor(private http: HttpClient) { }
+
+  getSomething():Observable<any>{
+    return this.http.get("http://localhost:3000/Project2/Servlet/getSomething");
+  }
+}
