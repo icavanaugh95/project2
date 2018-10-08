7c7861d00510f6244e1c7d9d10befa6d7f05f204
Cory <cory.davisalan@yahoo.com>
2018-10-08 13:41:05 -0400

    Finished Assess Batch Page Model
    Hopefully i got it all

--- src/main/java/pages/AssessBatchPage.java
+++ src/main/java/pages/AssessBatchPage.java
@@ -30,6 +30,9 @@ public class AssessBatchPage {
 	public List<WebElement> getYears() {
 		getYearSelector().click();
 
+		wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body > div > ui-view > ui-view > div:nth-child(1) > div > div.col-md-12.col-lg-12.top10 > ul:nth-child(1) > li.dropdown.open > ul > li:nth-child(1)")));
+
 		StringBuilder elementStr = new StringBuilder();
 		List<WebElement> webList = new ArrayList<WebElement>();
 		int i = 1;
@@ -170,7 +173,7 @@ public class AssessBatchPage {
 			webList.add(driver.findElement(By.cssSelector(elementStr.toString())));
 			elementStr.setLength(0);
 
-			
+
 
 		}
 
@@ -183,7 +186,7 @@ public class AssessBatchPage {
 	}
 
 
-	public List<WebElement> getAssociateTextBox(){
+	public List<WebElement> getAssociateNotesBox(){
 
 
 		StringBuilder elementStr = new StringBuilder();
@@ -212,5 +215,95 @@ public class AssessBatchPage {
 		return webList;
 
 	}
+
+	public List<WebElement> getAssociateRows(){
+
+
+		StringBuilder elementStr = new StringBuilder();
+		List<WebElement> webList = new ArrayList<WebElement>();
+		int i = 1;
+
+		while(true) {
+
+			elementStr.append("#trainer-assess-table > div > div > ul > ul > table > tbody > tr:nth-child(");
+			elementStr.append(i);
+			elementStr.append(")");
+			i += 1;
+			//System.out.println(elementStr.toString());
+
+			if(driver.findElements(By.cssSelector(elementStr.toString())).size() == 0)
+			{
+				break;
+			}
+
+			webList.add(driver.findElement(By.cssSelector(elementStr.toString())));
+			elementStr.setLength(0);
+
+
+		}
+
+		return webList;
+
+	}
+
+
+	public List<WebElement> getTraineeRowBoxes(WebElement row){
+
+		return row.findElements(By.tagName("td"));
+
+	}
+
+	public WebElement getShowCommentForm(int rowNum) {
+		List<WebElement> commentForm = driver.findElements(By.cssSelector("#trainer-assess-table > div > div > ul > ul > table > tbody > tr:nth-child("
+				+ (rowNum + 1) + ") > td:nth-child(2) > form"));
+
+		if(commentForm.size() > 0) {
+			return commentForm.get(0);
+		}
+
+		return null;
+	}
+	
+	
+	
+	public List<WebElement> getTableHeaders(){
+		return driver.findElements(By.cssSelector("#trainer-assess-table > div > div > ul > ul > table > thead > tr"));
+		
+	}
+	
+	public WebElement getEditAssessmentCategory(){
+		return driver.findElement(By.cssSelector("#category"));
+	}
+	
+	public WebElement getEditAssessmentMaxPoints() {
+		return driver.findElement(By.cssSelector("#editAssessmentModal_500000003 > div > div > div.modal-body > div > div:nth-child(2) > input"));
+		
+	}
+	
+	public WebElement getEditAssessmentType() {
+		return driver.findElement(By.cssSelector("#assessmentType"));
+	}
+	
+	public WebElement getEditAssessmentDelete() {
+		return driver.findElement(By.cssSelector("#editAssessmentModal_500000003 > div > div > div:nth-child(3) > div:nth-child(1) > button"));
+	}
+	
+	public WebElement getEditAssessmentUpdate() {
+		return driver.findElement(By.cssSelector("#editAssessmentModal_500000003 > div > div > div:nth-child(3) > div:nth-child(2) > input"));
+	}
+	
+	public WebElement getEditAssessmentClose() {
+		return driver.findElement(By.cssSelector("#editAssessmentModal_500000003 > div > div > div:nth-child(3) > div:nth-child(2) > button"));
+	}
+
+	public List<WebElement> getEditAssessmentCatagoryOptions(WebElement dropDown){
+		return dropDown.findElements(By.tagName("option"));
+	}
+	
+	public List<WebElement> getEditAssessmentTypeOptions(WebElement dropDown){
+		return dropDown.findElements(By.tagName("option"));
+	}
+
+
 
 }
