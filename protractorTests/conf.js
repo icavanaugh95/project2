28efe1dc713d2e79e483854df427b60eea5dac1b
Ian Cavanaugh <icavanaugh95@gmail.com>
2018-10-09 11:19:59 -0400

    added specs

--- protractorTests/conf.js
+++ protractorTests/conf.js
@@ -1,4 +1,4 @@
 exports.config = {
     seleniumAddress : "http://localhost:4444/wd/hub",
-    specs: ['locationsPageTest.js']
+    specs: ['login.js','locationsPageTests.js', 'categoryPageTests.js']
 };
