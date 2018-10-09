f2560bf4c65e69655a52ee00575c8bc761c68444
Ian Cavanaugh <icavanaugh95@gmail.com>
2018-10-09 11:21:32 -0400

    moved tests to specs folder

--- protractorTests/conf.js
+++ protractorTests/conf.js
@@ -1,4 +1,4 @@
 exports.config = {
     seleniumAddress : "http://localhost:4444/wd/hub",
-    specs: ['login.js','locationsPageTests.js', 'categoryPageTests.js']
+    specs: ['login.js','/specs/locationsPageTests.js', '/specs/categoryPageTests.js']
 };
