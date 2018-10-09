5000271611578023341103214c89faaf82119101
Ian Cavanaugh <icavanaugh95@gmail.com>
2018-10-09 13:24:14 -0400

    added jasmine timeout

--- protractorTests/conf.js
+++ protractorTests/conf.js
@@ -1,4 +1,15 @@
 exports.config = {
     seleniumAddress : "http://localhost:4444/wd/hub",
-    specs: ['login.js','specs/locationsPageTests.js', 'specs/categoryPageTests.js']
+    specs: ['login.js','specs/locationsPageTests.js', 'specs/categoryPageTests.js'],
+
+    capabilities: {
+        browserName: 'chrome'
+     },
+  
+     jasmineNodeOpts: {
+       showColors: true,
+       defaultTimeoutInterval: 360000
+     },
+  
+     allScriptsTimeout: 360000, 
 };
