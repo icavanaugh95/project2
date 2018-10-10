279ce0355eef358ca31c20ea456fcf078a5dd413
Ian Cavanaugh <icavanaugh95@gmail.com>
2018-10-09 21:07:06 -0400

    added request to run Quality Audit tests

--- src/main/java/requests/RequestHelper.java
+++ src/main/java/requests/RequestHelper.java
@@ -1,10 +1,16 @@
 package requests;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+import org.testng.collections.Lists;
 
 public class RequestHelper {
 	public static void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -13,5 +19,17 @@ public class RequestHelper {
 		
 		if(uri.equals("/Project2/Servlet/getSomething")) 
 			response.getWriter().append("[{\"name\":\"Adam\",\"Age\":19},{\"name\":\"Brian\",\"Age\":24},{\"name\":\"Jackie\",\"Age\":23}]");
+		else if(uri.equals("/Project2/Servlet/QualityAuditTests")) {
+			System.out.println("Here");
+			TestListenerAdapter adapter = new TestListenerAdapter();
+			TestNG testng = new TestNG();
+			List<String> suites = new ArrayList<String>();
+			
+			//path to xml
+			suites.add("C:\\Users\\Administrator\\.jenkins\\workspace\\Project 2\\src\\test\\resources\\testngAuditPage.xml"); 
+			testng.setVerbose(10);
+			testng.setTestSuites(suites);
+			testng.run();
+		}
 	}
 }
