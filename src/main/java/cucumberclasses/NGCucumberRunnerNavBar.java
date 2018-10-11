bcfd4eb1674fb516de88d6d89c151a0ea052d84c
Ian Cavanaugh <icavanaugh95@gmail.com>
2018-10-11 12:49:25 -0400

    fixed file paths to match ec2

--- src/main/java/cucumberclasses/NGCucumberRunnerNavBar.java
+++ src/main/java/cucumberclasses/NGCucumberRunnerNavBar.java
@@ -4,16 +4,16 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-		features = {"src/test/resources/NavBarAssessBatch.feature",
-					"src/test/resources/NavBarCategory.feature",
-					"src/test/resources/NavBarHome.feature",
-					"src/test/resources/NavBarLocations.feature",
-					"src/test/resources/NavBarManageBatch.feature",
-					"src/test/resources/NavBarPanel.feature",
-					"src/test/resources/NavBarQualityAudit.feature",
-					"src/test/resources/NavBarReports.feature",
-					"src/test/resources/NavBarSettings.feature",
-					"src/test/resources/NavBarTrainers.feature"},				
+		features = {"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarAssessBatch.feature",
+					"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarCategory.feature",
+					"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarHome.feature",
+					"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarLocations.feature",
+					"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarManageBatch.feature",
+					"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarPanel.feature",
+					"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarQualityAudit.feature",
+					"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarReports.feature",
+					"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarSettings.feature",
+					"C:/Users/Administrator/.jenkins/workspace/Project 2/src/test/resources/NavBarTrainers.feature"},				
 		glue = {"cucumberclasses"}
 		)
 
